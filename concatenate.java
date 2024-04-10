582a574efe32407cee0d8cc0ed1a2033baf88eaf
GaganSingh2 <gks022004@gmail.com>
2024-04-10 11:40:07 +0530

    Concatenate

--- concatenate.java
+++ concatenate.java
@@ -1,8 +1,9 @@
 public class concatenate {
     public static void main(String[] args) {
+        String s4 = "My fav. player name is ";
         String s1 = "Virat ";
         String s2 = "Kohli";
-        String s3 = s1 . concat(s2);
+        String s3 = s4 . concat(s1) . concat(s2);
         System.out.println(s3);
     }
     
